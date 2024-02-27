7b690fc49d0c4159b993ba9b1e2d902d8b7c07a9
joosinkim <ddclub86@gmail.com>
2024-02-27 19:48:51 +0900

    feat: table script 반영

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,34 @@
+create table `user`
+(
+    id                       bigint  auto_increment comment 'ID' primary key,
+    currency                 varchar(10)   not null comment '통화',
+    limit_amount             double        not null comment '한도 금액'
+);
+
+create table `merchant`
+(
+    id                       bigint  auto_increment comment 'ID' primary key,
+    fee_rate                 float   not null comment '수수료율'
+);
+
+create table `payment`
+(
+    id                       bigint  auto_increment comment 'ID' primary key,
+    user_id                  bigint        not null comment 'USER ID',
+    merchant_id              bigint        not null comment 'MERCHANT ID',
+    currency                 varchar(10)   not null comment '통화',
+    approve_amount           double        not null comment '승인 금액',
+    approve_datetime         datetime      not null comment '승인 일시',
+    foreign key (user_id)                  references `user`(id),
+    foreign key (merchant_id)              references `merchant`(id)
+);
+
+create table `payment_detail`
+(
+    id                       bigint  auto_increment comment 'ID' primary key,
+    payment_id               bigint        not null comment 'PAYMENT ID',
+    card_number              varchar(100)  not null comment '카드 번호',
+    expiry_date              char(4)       not null comment '만료 일자',
+    cvv                      char(3)       not null comment 'cvv',
+    foreign key (payment_id)               references `payment`(id)
+);
