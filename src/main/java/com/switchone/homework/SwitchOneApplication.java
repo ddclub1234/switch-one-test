ea5798e70f1fcf90f0fe49971e45a201f577cc91
joosinkim <ddclub86@gmail.com>
2024-02-27 14:38:52 +0900

    init

---
+++ src/main/java/com/switchone/homework/SwitchOneApplication.java
@@ -0,0 +1,13 @@
+package com.switchone.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwitchOneApplication.class, args);
+	}
+
+}
